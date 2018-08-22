a6e57846a03530a21a10c35dfd714f29f9064b5f
Petra Hadacova <p.hadacova@plus4u.net>
2018-08-22 13:46:17 +0200

    servis

--- BB_Banka/BB_Banka/Servisy/ServisBanka.cs
+++ BB_Banka/BB_Banka/Servisy/ServisBanka.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using BB_Banka.Models;
 
 namespace BB_Banka.Servisy
 {
     public class ServisBanka
     {
+        public void ZmenStavBroker(int id, int stav)
+        {
+           
+        }
     }
 }
