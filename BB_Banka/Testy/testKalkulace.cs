ffc322f2d3d04e514e148f7cc16acf7dd56e99c6
Viktor Polívka <viktor@polivka.local>
2018-08-23 14:35:31 +0200

    commit s testem

---
+++ BB_Banka/Testy/testKalkulace.cs
@@ -0,0 +1,31 @@
+﻿
+using BB_Banka;
+using NUnit.Framework;
+using System;
+
+
+namespace TestKalkulace
+{
+    [TestFixture]
+    class TestKalkulace
+    {
+        private ServisPozadavek pc;
+        private PozadPrijeti pp;
+        [SetUp]
+        public void nastav()
+        {
+            pc = new ServisPozadavek();
+            pp = new PozadPrijeti();
+        }
+        [Test]
+        public void testSpravneUdaje()
+        {
+
+          decimal vys=  pc.PridejPozadavky("777777777", "abcmail", 300002, 15, "Testuju", "Program", "pozn.", 1);
+            Assert.That(vys, Is.EqualTo((300000*ServisPozadavek.rpsn)/15));
+           
+
+        }
+
+    }
+}
